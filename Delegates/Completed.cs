bc2d2128320d4f32fc93060e93b6b5d28d8ecf6b
Veencee <110591295+Veencee@users.noreply.github.com>
2022-08-20 17:27:44 +0100

    Add files via upload

---
+++ Delegates/Completed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTask
+{
+    internal class Completed
+    {
+        //public delegate void (int name);
+       public delegate void Progress(int cyclesCompleted );
+       public static void clockCycles(Progress p)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var cycles = i.ToString();   
+                p(i);
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegateTask
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Write out the delegate for this expression
+            // var likelyUsername = t.FirstName + "." + t.LastName;
+            //var dUserCount = repoUser.WhereOne(x => x.UserName == likelyUsername);
+
+            //Two.use a delegate to create a callback for a long running task
+            Completed.clockCycles(ShowCompletedCycle);
+
+        }
+        public static void ShowCompletedCycle(int elapsedCycle)
+        {
+            Console.WriteLine($"{elapsedCycle} cycles elapsed");
+        }
+    }
+}
